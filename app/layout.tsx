6648eb69b643da07f6fdcf8a916a0af1e2bb28c3
Robert076 <robertberes06@gmail.com>
2025-10-29 16:57:36 +0200

    feat: setup

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import "./globals.css";
+
+const roboto = Roboto({
+  variable: "--font-roboto",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Electrician Oradea",
+  description: "Servicii electrice profesionale",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${roboto.variable} `}>{children}</body>
+    </html>
+  );
+}
