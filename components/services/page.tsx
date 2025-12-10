922ca567da612ccb26d9a041d8c0e50cdc875e98
Robert076 <robertberes06@gmail.com>
2025-12-10 16:06:15 +0200

    minor fixes

--- components/services/page.tsx
+++ components/services/page.tsx
@@ -6,32 +6,27 @@ const Services = () => {
   return (
     <div className="services">
       <h1>Servicii pe care le oferim</h1>
-      <div className="services-boxes">
+      <div className="services-container">
         <Service
           iconPath="/fulger.svg"
           title="Instalații electrice complete"
           text="Executăm instalații electrice complete pentru locuințe, birouri și spații comerciale. Asigurăm montajul, cablarea, măsurătorile și punerea în funcțiune conform normelor în vigoare, oferind siguranță și eficiență energetică."
         />
-
         <Service
           iconPath="/scut.svg"
           title="Reparații instalații electrice"
           text="Diagnosticăm rapid și reparam orice defecțiune electrică: prize, întrerupătoare, circuite arse sau suprasarcini. Folosim echipamente profesionale pentru intervenții sigure și durabile, indiferent de complexitatea lucrării."
         />
-
         <Service
           iconPath="/bec.svg"
           title="Montaj prize, întrerupătoare, corpuri de iluminat"
           text="Efectuăm montaj profesional pentru prize, întrerupătoare, lămpi, aplice și candelabre. Garanția noastră este un montaj sigur, curat și perfect funcțional, adaptat nevoilor și designului spațiului tău."
         />
-
         <Service
           iconPath="/cladire.svg"
           title="Înlocuire tablouri electrice & siguranțe"
           text="Modernizăm tablourile electrice vechi și înlocuim siguranțele pentru o distribuție eficientă și sigură a curentului. Folosim componente certificate, asigurând protecție maximă pentru echipamente și locuință."
         />
-      </div>
-      <div className="services-boxes-2">
         <Service
           iconPath="/constructor.svg"
           title="Verificări, depanare și diagnostic rapid"
